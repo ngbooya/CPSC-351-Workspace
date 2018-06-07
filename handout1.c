9d8cc11e0cfa1ca3c6f54b620619cfab80d32f42
Kevin Nguyen <ngbooya@gmail.com>
2018-06-07 10:44:44 -0700

    Delete code

--- handout1.c
+++ handout1.c
@@ -9,15 +9,5 @@ int main(){
 	fork();
 	fprintf(stderr, "Hello\n");*/
 
-	pid_t pid = fork();
-	if(pid == 0){
-		fprintf(stderr, "I will take a 100 second nap!");
-		sleep(100);
-
-		fprintf(stderr, "I am awake! Bye now.");
-		exit(0);
-	}else{
-		exit(1);
-	}
 
 }
