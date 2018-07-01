c543c193e627d10b15dee460adc0af67630331f6
Christian Angeles <christian.angeles@outlook.com>
2018-07-01 01:16:55 -0700

    removed some stuff

--- Assignment 2/MemoryManagerSim.cpp
+++ Assignment 2/MemoryManagerSim.cpp
@@ -84,14 +84,6 @@ void makePages(Processes mainProc[], int procIDbuff, pageObject pageTablebuff[],
 //remove pages from the page table when their time is up
 void removePages(Processes mainProc[], int procIDbuff, pageObject pageTablebuff[], int pageNumBuff, int timerBuff)
 {
-  int pageNumUsed = 0;
-
-  for(int i = 0; i < pageNumBuff; i++)
-  {
-    if(pageTablebuff[i].processID <= 0)
-      pageNumUsed++; //used for checking
-  }
-
   //checks number of free page and compares current timer with process (arriveTime + lifeTime) time
   if(((mainProc[procIDbuff-1].arriveTime + mainProc[procIDbuff-1].lifeTime) == timerBuff) && pageNumUsed > 0)
   {
@@ -102,7 +94,6 @@ void removePages(Processes mainProc[], int procIDbuff, pageObject pageTablebuff[
         pageTablebuff[i].memSizeBase = 0; //if checks above is true, then zero-out the info
         pageTablebuff[i].memSizeLimit = 0;
         pageTablebuff[i].processID = 0;
-        pageNumUsed--; //derements to reduce the for-loop checking; not all the pages will be used
       }
     }
   }
